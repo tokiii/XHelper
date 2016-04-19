5bd529cb9507affa3251ad192a9891a679bb1f1f
lost <wheheee@gmail.com>
2016-04-19 09:22:56 +0800

    修复业务展示年份不跟着滑动的bug

--- app/src/main/java/com/lost/cuthair/views/ClearEditText.java
+++ app/src/main/java/com/lost/cuthair/views/ClearEditText.java
@@ -16,18 +16,14 @@ import android.widget.EditText;
 import com.lost.cuthair.R;
 
 public class ClearEditText extends EditText implements  
-        OnFocusChangeListener, TextWatcher { 
-	/**
-	 * ɾ����ť������
-	 */
+        OnFocusChangeListener, TextWatcher {
     private Drawable mClearDrawable; 
  
     public ClearEditText(Context context) { 
     	this(context, null); 
     } 
  
-    public ClearEditText(Context context, AttributeSet attrs) { 
-    	//���ﹹ�췽��Ҳ����Ҫ����������ܶ����Բ�����XML���涨��
+    public ClearEditText(Context context, AttributeSet attrs) {
     	this(context, attrs, android.R.attr.editTextStyle); 
     } 
     
@@ -37,8 +33,7 @@ public class ClearEditText extends EditText implements
     }
     
     
-    private void init() { 
-    	//��ȡEditText��DrawableRight,����û���������Ǿ�ʹ��Ĭ�ϵ�ͼƬ
+    private void init() {
     	mClearDrawable = getCompoundDrawables()[2]; 
         if (mClearDrawable == null) { 
         	mClearDrawable = getResources() 
@@ -50,12 +45,7 @@ public class ClearEditText extends EditText implements
         addTextChangedListener(this); 
     } 
  
- 
-    /**
-     * ��Ϊ���ǲ���ֱ�Ӹ�EditText���õ���¼������������ü�ס���ǰ��µ�λ����ģ�����¼�
-     * �����ǰ��µ�λ�� ��  EditText�Ŀ�� - ͼ�굽�ؼ��ұߵļ�� - ͼ��Ŀ��  ��
-     * EditText�Ŀ�� - ͼ�굽�ؼ��ұߵļ��֮�����Ǿ�������ͼ�꣬��ֱ����û�п���
-     */
+
     @Override 
     public boolean onTouchEvent(MotionEvent event) { 
         if (getCompoundDrawables()[2] != null) { 
@@ -72,9 +62,7 @@ public class ClearEditText extends EditText implements
         return super.onTouchEvent(event); 
     } 
  
-    /**
-     * ��ClearEditText���㷢���仯��ʱ���ж������ַ��������������ͼ�����ʾ������
-     */
+
     @Override 
     public void onFocusChange(View v, boolean hasFocus) { 
         if (hasFocus) { 
@@ -85,20 +73,14 @@ public class ClearEditText extends EditText implements
     } 
  
  
-    /**
-     * �������ͼ�����ʾ�����أ�����setCompoundDrawablesΪEditText������ȥ
-     * @param visible
-     */
+
     protected void setClearIconVisible(boolean visible) { 
         Drawable right = visible ? mClearDrawable : null; 
         setCompoundDrawables(getCompoundDrawables()[0], 
                 getCompoundDrawables()[1], right, getCompoundDrawables()[3]); 
     } 
      
-    
-    /**
-     * ��������������ݷ����仯��ʱ��ص��ķ���
-     */
+
     @Override 
     public void onTextChanged(CharSequence s, int start, int count, 
             int after) { 
@@ -117,19 +99,13 @@ public class ClearEditText extends EditText implements
     } 
     
    
-    /**
-     * ���ûζ�����
-     */
+
     public void setShakeAnimation(){
     	this.setAnimation(shakeAnimation(5));
     }
     
     
-    /**
-     * �ζ�����
-     * @param counts 1���ӻζ�������
-     * @return
-     */
+
     public static Animation shakeAnimation(int counts){
     	Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
     	translateAnimation.setInterpolator(new CycleInterpolator(counts));
